9aaae155be25865f7f4886ac0380c97dde9ab0be
MikanChris <97997941+MikanChris@users.noreply.github.com>
2025-10-21 19:36:02 -0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:hello_world/main.dart'; // ← 如果 pubspec.yaml 的 name 不是 hello_world，改成你的包名
+import 'package:hello_world/main.dart';
 
 void main() {
   testWidgets('app builds', (WidgetTester tester) async {
